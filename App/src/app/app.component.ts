10d1b6e2aa4641a61298f6d88df353fa5a36ca57
David León <daviiidleongutierrez@gmail.com>
2025-03-27 14:58:27 +0100

    nuevo commit

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,4 +1,3 @@
-
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import {
@@ -7,32 +6,60 @@ import {
   IonMenu,
   IonContent,
   IonList,
-  IonListHeader,
-  IonNote,
   IonMenuToggle,
   IonItem,
   IonIcon,
   IonLabel,
   IonRouterOutlet,
   IonRouterLink,
-  IonButton, IonInput, IonImg
+  IonButton,
+  IonInput,
+  IonImg
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { shirtSharp ,logInSharp ,homeSharp ,mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import {
+  shirtSharp, logInSharp, homeSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp,
+  heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline,
+  warningSharp, bookmarkOutline, bookmarkSharp
+} from 'ionicons/icons';
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  imports: [RouterLink, RouterLinkActive, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterLink, IonRouterOutlet, IonButton, IonContent, IonInput, IonApp, IonContent, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonMenuToggle, IonNote, IonRouterOutlet, IonSplitPane, RouterLinkActive, IonImg],
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    IonApp,
+    IonSplitPane,
+    IonMenu,
+    IonContent,
+    IonList,
+    IonMenuToggle,
+    IonItem,
+    IonIcon,
+    IonLabel,
+    IonRouterLink,
+    IonRouterOutlet,
+    IonButton,
+    IonInput,
+    IonImg,
+    CabeceraComponent // 👈 Aquí importamos la cabecera
+  ],
 })
 export class AppComponent {
   public appPages = [
     { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Productos', url: '/product-list', icon: 'shirt' }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
   constructor() {
-    addIcons({ shirtSharp ,logInSharp,homeSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+    addIcons({
+      shirtSharp, logInSharp, homeSharp, mailOutline, mailSharp, paperPlaneOutline,
+      paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline,
+      trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp
+    });
   }
 }
---
+++ App/src/app/cabecera/cabecera.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cabecera',
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class CabeceraComponent {}
