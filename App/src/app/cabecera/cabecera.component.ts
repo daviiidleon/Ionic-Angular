bf0ca94882f8614beffb6ebf20ef2b6fe531a611
david <daviiidleongutierrez@gmail.com>
2025-03-31 12:00:30 +0200

    nuevo commit

--- App/src/app/cabecera/cabecera.component.ts
+++ App/src/app/cabecera/cabecera.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {IonMenuButton} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-cabecera',
   templateUrl: './cabecera.component.html',
   styleUrls: ['./cabecera.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonMenuButton],
 })
 export class CabeceraComponent {}
