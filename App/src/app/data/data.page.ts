e9fde6d43c72636081e9d403464212b276eb81b5
david <daviiidleongutierrez@gmail.com>
2025-04-09 10:43:06 +0200

    nuevo commit

--- App/src/app/data/data.page.ts
+++ App/src/app/data/data.page.ts
@@ -1,18 +1,25 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonContent, IonHeader, IonIcon, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {IonButton, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar} from '@ionic/angular/standalone';
 import {CabeceraComponent} from "../cabecera/cabecera.component";
 import {FooterComponent} from "../footer/footer.component";
 import {AuthService} from "../services/auth.service";
+import {
+  logOutOutline, personCircleOutline, cartOutline, search, cartSharp, shirtSharp,
+  logInSharp, homeSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp,
+  heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp,
+  warningOutline, warningSharp, bookmarkOutline, bookmarkSharp
+} from 'ionicons/icons';
 import {Router} from "@angular/router";
+import {addIcons} from "ionicons";
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.page.html',
   styleUrls: ['./data.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, CommonModule, FormsModule, CabeceraComponent, FooterComponent, IonIcon]
+  imports: [IonContent, IonHeader, CommonModule, FormsModule, CabeceraComponent, FooterComponent, IonIcon, IonButton]
 })
 export class DataPage implements OnInit {
   user: any = null;
@@ -21,7 +28,14 @@ export class DataPage implements OnInit {
     private authService: AuthService,
     private cd: ChangeDetectorRef,
     private router: Router
-  ) { }
+  ) {
+    addIcons({
+      logOutOutline, cartOutline, personCircleOutline, cartSharp, shirtSharp,
+      logInSharp, homeSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp,
+      heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp,
+      warningOutline, warningSharp, bookmarkOutline, bookmarkSharp
+    });
+  }
 
   ngOnInit() {
     this.authService.getAuthUserObservable().subscribe(user => {
@@ -48,5 +62,4 @@ export class DataPage implements OnInit {
       this.router.navigate(['/home']);
     });
   }
-
 }
--- App/src/app/services/firebase.service.ts
+++ App/src/app/services/firebase.service.ts
@@ -1,5 +1,67 @@
 // src/app/services/firebase.service.ts
+import { Injectable } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  User
+} from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc
+} from 'firebase/firestore';
+import { environment } from '../../environments/environment';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+  private auth;
+  private firestore;
+
+  constructor() {
+    const app = initializeApp(environment.firebaseConfig);
+    this.auth = getAuth(app);
+    this.firestore = getFirestore(app);
+  }
+
+  getAuth() {
+    return this.auth;
+  }
+
+  registerWithEmailAndPassword(email: string, password: string) {
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  loginWithEmailAndPassword(email: string, password: string) {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  signOut() {
+    return signOut(this.auth);
+  }
+
+  async saveUserData(uid: string, data: any) {
+    const userRef = doc(this.firestore, 'users', uid);
+    return await setDoc(userRef, data, { merge: true });
+  }
+
+  async getUserData(uid: string) {
+    const userRef = doc(this.firestore, 'users', uid);
+    const docSnap = await getDoc(userRef);
+    return docSnap.exists() ? docSnap.data() : null;
+  }
+}
+
+
+
+
+/*
+// src/app/services/firebase.service.ts
 import { Injectable } from '@angular/core';
 import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
@@ -38,3 +100,4 @@ export class FirebaseService {
     return signOut(this.auth);
   }
 }
+*/
