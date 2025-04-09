bc6fd458eaefe0e3f5cecd059816b0c208959105
david <daviiidleongutierrez@gmail.com>
2025-04-09 08:57:41 +0200

    nuevo commit

--- App/src/app/data/data.page.ts
+++ App/src/app/data/data.page.ts
@@ -1,22 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {IonContent, IonHeader, IonIcon, IonTitle, IonToolbar} from '@ionic/angular/standalone';
 import {CabeceraComponent} from "../cabecera/cabecera.component";
 import {FooterComponent} from "../footer/footer.component";
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.page.html',
   styleUrls: ['./data.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, CabeceraComponent, FooterComponent]
+  imports: [IonContent, IonHeader, CommonModule, FormsModule, CabeceraComponent, FooterComponent, IonIcon]
 })
 export class DataPage implements OnInit {
+  user: any = null;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private cd: ChangeDetectorRef,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuthUserObservable().subscribe(user => {
+      this.user = user;
+      this.cd.detectChanges();
+    });
+  }
+
+  goToMyData() {
+    this.router.navigate(['/data']); // ✅ Redirección a la página 'data'
+  }
+
+  goToMyPurchases() {
+    this.router.navigate(['/pay']);
+  }
+
+  goToMyFavorites() {
+    this.router.navigate(['/wishlist']);
+  }
+
+  logout() {
+    this.authService.logout().then(() => {
+      this.user = null;
+      this.router.navigate(['/home']);
+    });
   }
 
 }
